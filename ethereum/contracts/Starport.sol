286eb1c2b00dd2b53abf09c016a72ec01d882a94
maxwolff <maxcwolff@gmail.com>
2020-11-30 18:45:56 -0800

    test isMsgAuthorized, add most of changeAuthorities

--- ethereum/contracts/Starport.sol
+++ ethereum/contracts/Starport.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.7.5;
+pragma abicoder v2;
 
 import "./IERC20.sol";
 
@@ -19,13 +20,16 @@ contract Starport {
 
 	ICash immutable public cash;
 
+	bytes32 immutable public CHAIN_TYPE_ETH = keccak256(abi.encodePacked("ETH"));
 	address immutable public ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+	address[] public authorities;
 
 	event LockCash(address holder, uint amount, uint yieldIndex);
 	event Lock(address asset, address holder, uint amount);
 
-	constructor(ICash cash_) {
+	constructor(ICash cash_, address[] memory authorities_) {
 		cash = cash_;
+		authorities = authorities_;
 	}
 
 	function lock(uint amount, address asset) public {
@@ -64,6 +68,110 @@ contract Starport {
 
 	function transferInCash(address from, uint amount) internal {
 		require(cash.transferFrom(from, address(this), amount) == true, "TransferInCash");
+	}
+
+	// ** MESSAGES **
+	function changeAuthorities(bytes calldata changeAuthoritiesNotice, bytes[] calldata signatures) public {
+		// TODO: address[] memory authTemp = authorities;
+		// isMsgAuthorized(changeAuthoritiesNotice, authTemp, signatures);
+		verifyChainType(changeAuthoritiesNotice);
+
+		uint CHAIN_TYPE_BYTES = 3;
+		uint WORD_SIZE = 32;
+
+		uint len = sub(changeAuthoritiesNotice.length, CHAIN_TYPE_BYTES);
+		require(len % WORD_SIZE == 0, "Excess bytes");
+		uint numAuths = len / WORD_SIZE;
+
+		address[] memory newAuths = new address[](numAuths);
+		for (uint i = 0; i < numAuths; i ++) {
+			uint startIdx = add(CHAIN_TYPE_BYTES, mul(i, WORD_SIZE));
+			uint endIdx = add(startIdx, WORD_SIZE);
+			address newAuth = abi.decode(changeAuthoritiesNotice[startIdx:endIdx], (address));
+			newAuths[i] = newAuth;
+		}
+		authorities = newAuths;
+		// TODO: Let authHash = hash(newAuthorities)
+		// Emit ChangeAuthorities(authHash: bytes32)
+	}
+
+
+	function verifyChainType(bytes calldata notice) public view returns (bool) {
+		//ABI Decoding requires padded values, so we have to grab bytes3 manually and pass the full memory slot for addresses
+		// https://solidity.readthedocs.io/en/latest/types.html?highlight=slice#array-slices
+		bytes3 chainType =
+			notice[0] |
+            (bytes3(notice[1]) >> 8) |
+            (bytes3(notice[2]) >> 16);
+
+        require(keccak256(abi.encodePacked(chainType)) == CHAIN_TYPE_ETH, "Invalid Sig");
+	}
+
+	function getAuthorities() public view returns (address[] memory){
+		return authorities;
+	}
+
+	// ** PURE HELPERS **
+
+
+	// @dev Reverts if message is not authorized
+	function isMsgAuthorized(
+		bytes calldata message,
+		address[] memory authorities_,
+		bytes[] memory signatures
+	) public pure returns (bool) {
+		address[] memory sigs = new address[](signatures.length);
+		for (uint i = 0; i < signatures.length; i++) {
+			address signer = recover(keccak256(message), signatures[i]);
+			require(contains(sigs, signer) == false, "Duplicated sig");
+			require(contains(authorities_, signer) == true, "Unauthorized signer");
+			sigs[i] = signer;
+		}
+		require(sigs.length >= getQuorum(authorities_.length), "Below quorum threshold");
+		return true;
+	}
+
+	function contains(address[] memory arr, address elem) internal pure returns (bool) {
+		for(uint i = 0; i < arr.length; i++) {
+			if (arr[i] == elem) return true;
+		}
+		return false;
+	}
+
+	// @dev Quorum is > 1/3 authorities approving
+	function getQuorum(uint authorityCount) public pure returns (uint) {
+		return (authorityCount / 3) + 1;
+	}
+
+
+	// @dev Adapted from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/ECDSA.sol
+	function recover(bytes32 hash, bytes memory signature) public pure returns (address) {
+	    // Check the signature length
+	    if (signature.length != 65) {
+	        revert("ECDSA: invalid signature length");
+	    }
+
+	    // Divide the signature in r, s and v variables
+	    bytes32 r;
+	    bytes32 s;
+	    uint8 v;
+
+	    // ecrecover takes the signature parameters, and the only way to get them
+	    // currently is to use assembly.
+	    // solhint-disable-next-line no-inline-assembly
+	    assembly {
+	        r := mload(add(signature, 0x20))
+	        s := mload(add(signature, 0x40))
+	        v := byte(0, mload(add(signature, 0x60)))
+	    }
+
+	    require(v == 27 || v == 28, "ECDSA: invalid signature 'v' value");
+
+	    // If the signature is valid (and not malleable), return the signer address
+	    address signer = ecrecover(hash, v, r, s);
+	    require(signer != address(0), "ECDSA: invalid signature");
+
+	    return signer;
 	}
 
 	receive() external payable {
--- ethereum/tests/StarportTest.js
+++ ethereum/tests/StarportTest.js
@@ -1,5 +1,6 @@
 const RLP = require('rlp');
 const {ethers} = require('ethers');
+const {Wallet} = require("@ethersproject/wallet");
 
 const bi = num => {
   return BigInt(num);
@@ -13,6 +14,22 @@ const getHypotheticalAddr = (acct, nonce) => {
   return '0x' + web3.utils.sha3(RLP.encode([acct, nonce])).slice(12).substring(14);
 }
 
+let createAccounts = (num) => {
+  return Array(num).fill(null).map(() => Wallet.createRandom());
+}
+
+// No ethereum prefix, just signs the hash of the raw digest
+const sign = (msg, account) => {
+  const hash = ethers.utils.keccak256(msg);
+  const sk = new ethers.utils.SigningKey(account.privateKey);
+  const hashArr = ethers.utils.arrayify(hash);
+  const sigRaw = sk.signDigest(hashArr);
+  const sig = ethers.utils.joinSignature(sigRaw);
+
+  return {hash, sig}
+}
+
+
 const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 
 const getLockEventTopic = () => {
@@ -25,18 +42,84 @@ describe('Starport', () => {
   let starport;
   let cash;
   let tokenA;
-  let [root, a1, a2] = saddle.accounts;
+  const authorityAccts = createAccounts(3);
+  const authorityAddrs = authorityAccts.map(acct => acct.address);
+  let [root, a1] = saddle.accounts;
+  const testMsg = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+
 
   beforeEach(async () => {
     const nonce = await web3.eth.getTransactionCount(root);
     const starportAddr = getHypotheticalAddr(accounts[0], nonce);
     const cashAddr = getHypotheticalAddr(accounts[0], nonce + 1);
-    starport = await deploy('Starport', [cashAddr]);
+    starport = await deploy('Starport', [cashAddr, authorityAddrs]);
     cash = await deploy('MockCashToken', [starportAddr, mantissa(100), a1]);
 
     tokenA = await deploy('FaucetToken', [mantissa(100), "tokenA", 18, "TKNA"]);
     await send(tokenA, "allocateTo", [a1, mantissa(10)]);
     await send(tokenA, "approve", [starport._address, mantissa(10)], { from: a1 });
+  });
+
+  it('should calculate quorum correctly', async () => {
+    const testQuorum = async (numAuth, quorum) => expect(await call(starport, 'getQuorum',[numAuth])).numEquals(quorum);
+    await testQuorum(1,1);
+    await testQuorum(3,2);
+    await testQuorum(5,2);
+    await testQuorum(6,3);
+  });
+
+  it('should recover signer', async () => {
+    const acct = authorityAccts[0];
+    const {hash, sig} = sign(testMsg, acct);
+    const signer = await call(starport, 'recover', [hash, sig]);
+    expect(signer).toBe(acct.address);
+  });
+
+  it('should authorize message', async () => {
+    const sigs = authorityAccts.map(acct => sign(testMsg, acct).sig);
+    expect(await call(starport, 'isMsgAuthorized', [testMsg, authorityAddrs, sigs])).toBe(true);
+  });
+
+  it('should not authorize duplicate sigs', async () => {
+    const dupAccts = Array(3).fill(authorityAccts[0]);
+    const sigs = dupAccts.map(acct => sign(testMsg, acct).sig);
+    await expect(call(starport, 'isMsgAuthorized', [testMsg, authorityAddrs, sigs])).rejects.toRevert('revert Duplicated sig');
+  });
+
+  it('should not authorize with too few sigs', async () => {
+    const acct = authorityAccts[0];
+    const sig = sign(testMsg, acct).sig;
+    await expect(call(starport, 'isMsgAuthorized', [testMsg, authorityAddrs, [sig]])).rejects.toRevert('revert Below quorum threshold');
+  });
+
+  it('should not authorize with unauthorized signer', async () => {
+    const badAccts = createAccounts(2);
+    const sigs = badAccts.map(acct => sign(testMsg, acct).sig);
+    await expect(call(starport, 'isMsgAuthorized', [testMsg, authorityAddrs, sigs])).rejects.toRevert('revert Unauthorized signer');
+  });
+
+
+  const ethChainType = () => {
+    const byteArr = ethers.utils.toUtf8Bytes("ETH");
+    const decoded = ethers.utils.defaultAbiCoder.encode(['bytes3'], [byteArr]);
+    return decoded.slice(2).substring(6,0);
+  }
+
+  it('should change authorities', async ()=> {
+    const chainTypeBytes = ethChainType();
+
+    const LEN = 2;
+    const newAuths = createAccounts(LEN).map(acct => acct.address);
+    const paramTypes = Array(LEN).fill('address');
+    const encodedAddrs = ethers.utils.defaultAbiCoder.encode(paramTypes, newAuths).slice(2);
+    const notice = "0x" + chainTypeBytes + encodedAddrs;
+
+    const sigs = authorityAccts.map(acct => sign(notice, acct).sig);
+
+    await send(starport, 'changeAuthorities', [notice, sigs]);
+    const authAfter = await call(starport, 'getAuthorities');
+    expect(authAfter).toEqual(newAuths);
+
   });
 
   it('should have correct references', async () => {
