38a51a9a87a5d502277df47e8ae145d3e99139a6
Jared Flatow <jflatow@gmail.com>
2020-12-14 11:56:05 -0800

    Notice types idea (#43)

--- pallets/cash/src/chains.rs
+++ pallets/cash/src/chains.rs
@@ -1,5 +1,4 @@
 // XXX where should this live? with e.g. ethereum_client?
-
 pub mod eth {
     // Note: The substrate build requires these be imported
     pub use sp_std::vec::Vec;
@@ -27,4 +26,78 @@ pub mod eth {
             ethabi::token::Token::Int(log_index.into()),
         ])
     }
+}
+
+pub type Amount = u128; // XXX not really
+pub type Index = u128; // XXX
+pub type Rate = u128; // XXX
+pub type Timestamp = u32; // XXX
+
+pub type GenerationId = u32;
+pub type WithinGenerationId = u32;
+pub type NoticeId = (GenerationId, WithinGenerationId);
+
+pub trait L1 {
+    type Address = [u8; 20];
+    type Account = Self::Address;
+    type Asset = Self::Address;
+    type Hash = [u8; 32];
+    type Public = [u8; 32];
+}
+
+#[derive(Debug)]
+pub struct Ethereum {}
+
+#[derive(Debug)]
+pub struct Polkadot {}
+
+#[derive(Debug)]
+pub struct Solana {}
+
+#[derive(Debug)]
+pub struct Tezos {}
+
+impl L1 for Ethereum {}
+impl L1 for Polkadot {}
+impl L1 for Solana {}
+impl L1 for Tezos {}
+
+#[derive(Debug)]
+pub enum Notice<'a, Chain: L1> {
+    ExtractionNotice {
+        id: NoticeId,
+        parent: Chain::Hash,
+        asset: Chain::Asset,
+        account: Chain::Account,
+        amount: Amount,
+    },
+
+    CashExtractionNotice {
+        id: NoticeId,
+        parent: Chain::Hash,
+        account: Chain::Asset,
+        amount: Chain::Account,
+        cash_yield_index: Index,
+    },
+
+    FutureYieldNotice {
+        id: NoticeId,
+        parent: Chain::Hash,
+        next_cash_yield: Rate,
+        next_cash_yield_start_at: Timestamp,
+        next_cash_yield_index: Index,
+    },
+
+    SetSupplyCapNotice {
+        id: NoticeId,
+        parent: Chain::Hash,
+        asset: Chain::Asset,
+        amount: Amount,
+    },
+
+    ChangeAuthorityNotice {
+        id: NoticeId,
+        parent: Chain::Hash,
+        new_authorities: &'a [Chain::Public],
+    },
 }
