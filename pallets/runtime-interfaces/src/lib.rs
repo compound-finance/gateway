6a4c722ea1e6a21aa892a98d31f2d1583a275277
Wayne Nilsen <wayne@compound.finance>
2020-12-16 08:27:46 -0500

    Separate validator specific configuration runtime interface
    The comments in the code generally summarize why this is necessary and what it is. One question that I think is open to debate is "Why use a separate runtime interface?". The answer there is that I did not want any confusion on behalf of the calling side of this code now or in the future. It naturally lends itself to a separate interface with separate functionality. This helps keep the two types of configuration separated in the developer mind. Another question that I think is worth asking is "Why not move eth_rpc_url into the new world?". The answer there is mainly just to avoid being disruptive to those already happily using the code possibly breaking local-dev setups as they exist today.
    
    I was thinking about adding a dot env file but at this point I think that the environment variables that we will be using for configuration should be fairly self documented right here in this module and there should not be a documentation issue.

--- pallets/runtime-interfaces/src/lib.rs
+++ pallets/runtime-interfaces/src/lib.rs
@@ -48,6 +48,11 @@ lazy_static! {
     static ref CONFIG: Mutex<Config> = Mutex::new(new_config("".into(), "".into(), "".into()));
 }
 
+/// The configuration interface for offchain workers. This is designed to manage configuration
+/// that is specific to compound chain AND distributed in the chain spec file. Ultimately
+/// the things that are being configured here should not be changed by any honest validators.
+/// Each chain spec file will have different values for these configurations by design, for example
+/// testnet may point to an Ethereum testnet starport address and lock event topic.
 #[sp_runtime_interface::runtime_interface]
 pub trait ConfigInterface {
     /// This method is designed to be used by the node as it starts up. The node reads
@@ -62,6 +67,29 @@ pub trait ConfigInterface {
     /// the configuration to determine where to make RPC calls among other parameters.
     fn get() -> Config {
         CONFIG.lock().unwrap().clone()
+    }
+}
+
+const ETH_KEY_ID_ENV_VAR: &str = "ETH_KEY_ID";
+
+/// The ValidatorConfigInterface is designed to be modified as needed by the validators. This means
+/// that each validator should be modifying the values here. For example, the ETH_KEY_ID is set
+/// by each validator separately corresponding to their HSM configuration and key ID that they
+/// have set up on the backend. This has nothing to do with the "chain spec" file and thus is
+/// a separate interface to avoid issues with validators having to modify the chain spec file
+/// perhaps becoming byzantine in the process via human error.
+///
+/// It is also not a file based configuration but an environment variable based configuration.
+/// This allows for more straightforward configuration in some cases and separates values that
+/// are integral to the system from validator specific values that otherwise would be intermingled.
+#[sp_runtime_interface::runtime_interface]
+pub trait ValidatorConfigInterface {
+    /// Get the Key ID for the Ethereum key.
+    ///
+    /// Downstream this is used to feed the keyring for signing and obtaining the corresponding
+    /// public key.
+    fn get_eth_key_id() -> Option<Vec<u8>> {
+        std::env::var(ETH_KEY_ID_ENV_VAR).ok().map(Into::into)
     }
 }
 
