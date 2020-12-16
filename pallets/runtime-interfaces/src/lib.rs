6f1ad2ef30297d7a3e787ef9e4b9b3f5d7506817
Wayne Nilsen <waynenilsen@gmail.com>
2020-12-16 15:03:59 -0500

    Separate validator specific configuration runtime interface (#51)
    * Separate validator specific configuration runtime interface
    
    The comments in the code generally summarize why this is necessary and what it is. One question that I think is open to debate is "Why use a separate runtime interface?". The answer there is that I did not want any confusion on behalf of the calling side of this code now or in the future. It naturally lends itself to a separate interface with separate functionality. This helps keep the two types of configuration separated in the developer mind. Another question that I think is worth asking is "Why not move eth_rpc_url into the new world?". The answer there is mainly just to avoid being disruptive to those already happily using the code possibly breaking local-dev setups as they exist today.
    
    I was thinking about adding a dot env file but at this point I think that the environment variables that we will be using for configuration should be fairly self documented right here in this module and there should not be a documentation issue.
    
    * move ETH_RPC_URL to an environment variable sooner rather than later
    
    * fix test

--- pallets/runtime-interfaces/src/lib.rs
+++ pallets/runtime-interfaces/src/lib.rs
@@ -6,7 +6,6 @@ use std::sync::Mutex;
 
 #[derive(Clone, PassByCodec, codec::Encode, codec::Decode)]
 pub struct Config {
-    eth_rpc_url: Vec<u8>,
     eth_starport_address: Vec<u8>,
     eth_lock_event_topic: Vec<u8>,
 }
@@ -14,13 +13,8 @@ pub struct Config {
 /// XXX Possible sanity checks for config fields here
 impl Config {
     pub fn update(&mut self, new: Config) {
-        self.eth_rpc_url = new.eth_rpc_url;
         self.eth_starport_address = new.eth_starport_address;
         self.eth_lock_event_topic = new.eth_lock_event_topic;
-    }
-
-    pub fn get_eth_rpc_url(&self) -> Vec<u8> {
-        self.eth_rpc_url.clone()
     }
 
     pub fn get_eth_starport_address(&self) -> Vec<u8> {
@@ -32,22 +26,22 @@ impl Config {
     }
 }
 
-pub fn new_config(
-    eth_rpc_url: Vec<u8>,
-    eth_starport_address: Vec<u8>,
-    eth_lock_event_topic: Vec<u8>,
-) -> Config {
+pub fn new_config(eth_starport_address: Vec<u8>, eth_lock_event_topic: Vec<u8>) -> Config {
     return Config {
-        eth_rpc_url: eth_rpc_url,
         eth_starport_address: eth_starport_address,
         eth_lock_event_topic: eth_lock_event_topic,
     };
 }
 
 lazy_static! {
-    static ref CONFIG: Mutex<Config> = Mutex::new(new_config("".into(), "".into(), "".into()));
+    static ref CONFIG: Mutex<Config> = Mutex::new(new_config("".into(), "".into()));
 }
 
+/// The configuration interface for offchain workers. This is designed to manage configuration
+/// that is specific to compound chain AND distributed in the chain spec file. Ultimately
+/// the things that are being configured here should not be changed by any honest validators.
+/// Each chain spec file will have different values for these configurations by design, for example
+/// testnet may point to an Ethereum testnet starport address and lock event topic.
 #[sp_runtime_interface::runtime_interface]
 pub trait ConfigInterface {
     /// This method is designed to be used by the node as it starts up. The node reads
@@ -65,6 +59,36 @@ pub trait ConfigInterface {
     }
 }
 
+const ETH_KEY_ID_ENV_VAR: &str = "ETH_KEY_ID";
+
+const ETH_RPC_URL_ENV_VAR: &str = "ETH_RPC_URL";
+
+/// The ValidatorConfigInterface is designed to be modified as needed by the validators. This means
+/// that each validator should be modifying the values here. For example, the ETH_KEY_ID is set
+/// by each validator separately corresponding to their HSM configuration and key ID that they
+/// have set up on the backend. This has nothing to do with the "chain spec" file and thus is
+/// a separate interface to avoid issues with validators having to modify the chain spec file
+/// perhaps becoming byzantine in the process via human error.
+///
+/// It is also not a file based configuration but an environment variable based configuration.
+/// This allows for more straightforward configuration in some cases and separates values that
+/// are integral to the system from validator specific values that otherwise would be intermingled.
+#[sp_runtime_interface::runtime_interface]
+pub trait ValidatorConfigInterface {
+    /// Get the Key ID for the Ethereum key.
+    ///
+    /// Downstream this is used to feed the keyring for signing and obtaining the corresponding
+    /// public key.
+    fn get_eth_key_id() -> Option<Vec<u8>> {
+        std::env::var(ETH_KEY_ID_ENV_VAR).ok().map(Into::into)
+    }
+
+    /// Get the Ethereum node RPC URL
+    fn get_eth_rpc_url() -> Option<Vec<u8>> {
+        std::env::var(ETH_RPC_URL_ENV_VAR).ok().map(Into::into)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -72,9 +96,6 @@ mod tests {
     #[test]
     fn test_basic() {
         // this gets loaded from the json file "chain config file"
-        let given_eth_rpc_url: Vec<u8> = "my eth rpc url".into();
-        let expected_eth_rpc_url: Vec<u8> = "my eth rpc url".into();
-
         let given_eth_starport_address: Vec<u8> =
             "0xbbde1662bC3ED16aA8C618c9833c801F3543B587".into();
         let expected_eth_starport_address: Vec<u8> =
@@ -86,7 +107,6 @@ mod tests {
             "0xec36c0364d931187a76cf66d7eee08fad0ec2e8b7458a8d8b26b36769d4d13f3".into();
 
         let config = new_config(
-            given_eth_rpc_url.clone(),
             given_eth_starport_address.clone(),
             given_eth_lock_event_topic.clone(),
         );
@@ -94,10 +114,8 @@ mod tests {
         config_interface::set(config);
         // ...later... in offchain worker context, get the configuration
         let actual_config = config_interface::get();
-        let actual_eth_rpc_url = actual_config.eth_rpc_url;
         let actual_eth_starport_address = actual_config.eth_starport_address;
         let actual_eth_lock_event_topic = actual_config.eth_lock_event_topic;
-        assert_eq!(expected_eth_rpc_url, actual_eth_rpc_url);
         assert_eq!(expected_eth_starport_address, actual_eth_starport_address);
         assert_eq!(expected_eth_lock_event_topic, actual_eth_lock_event_topic);
     }
