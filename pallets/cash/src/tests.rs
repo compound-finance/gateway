1ee1df328797f3597a60f793d7059cce12786eda
Wayne Nilsen <wayne@compound.finance>
2020-12-29 13:55:45 -0500

    fix test by initializing the validators

--- pallets/cash/src/tests.rs
+++ pallets/cash/src/tests.rs
@@ -36,9 +36,17 @@ fn it_magically_extracts() {
     });
 }
 
+fn initialize_validators() {
+    CashModule::initialize_validators(vec![
+        "0458bfa2eec1cd8f451b41a1ad1034614986a6e65eabe24b5a7888d3f7422d6130e35d36561b207b1f9462bd8a982bd5b5204a2f8827b38469841ef537554ff1ba".into(),
+        "04c3e5ff2cb194d58e6a51ffe2df490c70d899fee4cdfff0a834fcdfd327a1d1bdaae3f1719d7fd9a9ee4472aa5b14e861adef01d9abd44ce82a85e19d6e21d3a4".into()
+    ]);
+}
+
 #[test]
 fn process_eth_event_happy_path() {
     new_test_ext().execute_with(|| {
+        initialize_validators();
         // Dispatch a signed extrinsic.
         // XXX
         let payload = vec![
