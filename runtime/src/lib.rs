3f5f2afb37ed9be7323452395c16ba598fb1afb9
Geoff Hayes <hayesgm@gmail.com>
2021-01-06 21:37:47 -0800

    Remove Syn Dep Peg (#80)
    * Remove Syn Dep Peg
    
    This patch removes the peg for the `syn` package, which has been fixed in substrate master and merged into our substrate branch [here](https://github.com/compound-finance/substrate/pull/7).
    
    * Implement next epoch

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -504,6 +504,10 @@ impl_runtime_apis! {
         Babe::current_epoch()
     }
 
+    fn next_epoch() -> sp_consensus_babe::Epoch {
+        Babe::next_epoch()
+    }
+
     fn generate_key_ownership_proof(
         _slot_number: sp_consensus_babe::SlotNumber,
         _authority_id: sp_consensus_babe::AuthorityId,
