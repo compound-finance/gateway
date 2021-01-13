c2571d9243d879e9cc8ac5389e1a965d8e3e5a5b
Geoffrey Hayes <hayesgm@gmail.com>
2021-01-12 23:32:27 -0800

    Add Trx Requests
    This patch adds trx requests, as in `(magic-extract max eth:0x...)`, which will be properly signed by user-accounts instead of calling Compound Chain extrinsics directly. We use an off-the-shelf lexer and then build our own (very very simple) parser. We currently only support magic extract, but it should be rather easy to add new functions to the `TrxRequest` enum. Each function _can_ use its own custom parser for its arguments though (e.g. magic extract allows `max` in lieu of a specific amount). The biggest TODO left on this topic will be using real signatures for "from" instead of magic extract's ignorance of the msg sender.

---
+++ pallets/cash/src/trx_req.rs
@@ -0,0 +1,42 @@
+extern crate trx_request;
+
+use crate::chains::*;
+use crate::core::*;
+use trx_request::*;
+
+pub fn max_amount_to_generic(max_amount: MaxAmount, if_max: &Fn() -> GenericQty) -> GenericQty {
+  match max_amount {
+    MaxAmount::Amt(amt) => amt,
+    MaxAmount::Max => if_max(),
+  }
+}
+
+pub fn account_to_generic(account: trx_request::Account) -> GenericAccount {
+  match account {
+    trx_request::Account::Eth(address) => (ChainId::Eth, address.into()),
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_max_amount_to_generic() {
+    assert_eq!(max_amount_to_generic(MaxAmount::Amt(5), &|| 88), 5);
+    assert_eq!(max_amount_to_generic(MaxAmount::Max, &|| 88), 88);
+  }
+
+  #[test]
+  fn test_account_to_generic() {
+    assert_eq!(
+      account_to_generic(Account::Eth([
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+      ])),
+      (
+        ChainId::Eth,
+        vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      )
+    );
+  }
+}
