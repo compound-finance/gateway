5e47e685800c073840a4b0651cdb6c4da08cf3c3
Geoffrey Hayes <hayesgm@gmail.com>
2021-01-14 15:15:50 -0800

    Cargo fmt

--- trx-request/src/lex.rs
+++ trx-request/src/lex.rs
@@ -2,54 +2,54 @@ use logos::{Lexer, Logos};
 
 #[derive(Logos, Debug, PartialEq, Eq, Clone)]
 pub enum Token<'a> {
-  #[token("(")]
-  LeftDelim,
+    #[token("(")]
+    LeftDelim,
 
-  #[token(")")]
-  RightDelim,
+    #[token(")")]
+    RightDelim,
 
-  #[regex(r"0x[0-9a-fA-F]+", parse_hex)]
-  Hex(Option<Vec<u8>>),
+    #[regex(r"0x[0-9a-fA-F]+", parse_hex)]
+    Hex(Option<Vec<u8>>),
 
-  #[regex(r"[0-9]+", parse_int)]
-  Integer(Option<u128>),
+    #[regex(r"[0-9]+", parse_int)]
+    Integer(Option<u128>),
 
-  #[regex(r"[a-zA-Z-]+")]
-  Identifier(&'a str),
+    #[regex(r"[a-zA-Z-]+")]
+    Identifier(&'a str),
 
-  #[regex(r"[a-zA-Z0-9]+:[a-zA-Z0-9]+", split_pair)]
-  Pair(Option<(&'a str, &'a str)>),
+    #[regex(r"[a-zA-Z0-9]+:[a-zA-Z0-9]+", split_pair)]
+    Pair(Option<(&'a str, &'a str)>),
 
-  #[regex(r"[ \t\n\f]+", logos::skip)]
-  Whitespace,
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Whitespace,
 
-  #[error]
-  Error,
+    #[error]
+    Error,
 }
 
 fn split_pair<'a>(lex: &mut Lexer<'a, Token<'a>>) -> Option<(&'a str, &'a str)> {
-  lex.slice().split_once(':')
+    lex.slice().split_once(':')
 }
 
 fn parse_hex<'a>(lex: &mut Lexer<'a, Token<'a>>) -> Option<Vec<u8>> {
-  hex::decode(&lex.slice()[2..]).ok()
+    hex::decode(&lex.slice()[2..]).ok()
 }
 
 fn parse_int<'a>(lex: &mut Lexer<'a, Token<'a>>) -> Option<u128> {
-  u128::from_str_radix(&lex.slice()[..], 10).ok()
+    u128::from_str_radix(&lex.slice()[..], 10).ok()
 }
 
 pub fn lex<'source>(text: &'source str) -> Lexer<'source, Token> {
-  Token::lexer(text)
+    Token::lexer(text)
 }
 
 // TODO: handle lowercase
 // TODO: handle odd-length hex numbers?
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  macro_rules! lex_tests {
+    macro_rules! lex_tests {
     ($($name:ident: $input:expr => $exp:expr,)*) => {
     $(
         #[test]
@@ -63,27 +63,27 @@ mod tests {
     }
   }
 
-  lex_tests! {
-    invalid_chars:
-      "(hi!" => vec![Token::LeftDelim, Token::Identifier("hi"), Token::Error],
-    invalid_unicode_chars:
-      "(touché 50)" => vec![
-        Token::LeftDelim,
-        Token::Identifier("touch"),
-        Token::Error,
-        Token::Integer(Some(50)),
-        Token::RightDelim
-      ],
-    simple_lex:
-      "()" => vec![Token::LeftDelim, Token::RightDelim],
-    simple_fun_call:
-      "(my-fun 55 0x0100 eth:0x20)" => vec![
-        Token::LeftDelim,
-        Token::Identifier("my-fun"),
-        Token::Integer(Some(55)),
-        Token::Hex(Some(vec![1, 0])),
-        Token::Pair(Some(("eth", "0x20"))),
-        Token::RightDelim
-      ],
-  }
+    lex_tests! {
+      invalid_chars:
+        "(hi!" => vec![Token::LeftDelim, Token::Identifier("hi"), Token::Error],
+      invalid_unicode_chars:
+        "(touché 50)" => vec![
+          Token::LeftDelim,
+          Token::Identifier("touch"),
+          Token::Error,
+          Token::Integer(Some(50)),
+          Token::RightDelim
+        ],
+      simple_lex:
+        "()" => vec![Token::LeftDelim, Token::RightDelim],
+      simple_fun_call:
+        "(my-fun 55 0x0100 eth:0x20)" => vec![
+          Token::LeftDelim,
+          Token::Identifier("my-fun"),
+          Token::Integer(Some(55)),
+          Token::Hex(Some(vec![1, 0])),
+          Token::Pair(Some(("eth", "0x20"))),
+          Token::RightDelim
+        ],
+    }
 }
