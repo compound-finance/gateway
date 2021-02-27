8c542906187b69bec1c2a13bc60794a44c49dc6f
Coburn Berry <coburn@compound.finance>
2021-02-26 21:54:34 -0800

    bunch of stuff about deploying ropsten

--- scripts/build_spec.sh
+++ scripts/build_spec.sh
@@ -8,4 +8,7 @@ cd $(dirname ${BASH_SOURCE[0]})/..
 
 cargo build --release
 ./target/release/compound-chain build-spec --disable-default-bootnode --chain staging > alphaTestnetChainSpec.json
-./target/release/compound-chain build-spec --chain=alphaTestnetChainSpec.json --raw --disable-default-bootnode > alphaTestnetChainSpecRaw.json
+#todo: just put it in chain_spec.rs...
+cat alphaTestnetChainSpec.json | jq --slurpfile ass ./scripts/chains/ropsten_token_config.json 'setpath(["genesis", "runtime", "palletCash", "assets"]; $ass)' > m.json
+./target/release/compound-chain build-spec --chain=m.json --raw --disable-default-bootnode > alphaTestnetChainSpecRaw.json
+rm m.json
