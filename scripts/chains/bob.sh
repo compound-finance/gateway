fd775547cb7ad0cd616c26adf6fe3095b04cee6f
Coburn Berry <coburn@compound.finance>
2021-02-28 07:27:23 -0800

    built

--- scripts/chains/bob.sh
+++ scripts/chains/bob.sh
@@ -17,7 +17,7 @@ if [ "$purge" = true ] ; then
 fi
 
 export ETH_KEY_ID=my_eth_key_id
-export ETH_RPC_URL=https://goerli.infura.io/v3/975c0c48e2ca4649b7b332f310050e27
+export ETH_RPC_URL=https://ropsten-eth.compound.finance
 export MINER="ETH:0x66613A2d4908D130C908ccF2f298b235bACD427a"
 ./target/release/compound-chain \
   --base-path /tmp/chainz/bob \
