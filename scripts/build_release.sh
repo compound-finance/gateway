75f22cd8e364b14b553cd3737ccf821263c19164
Geoffrey Hayes <hayesgm@gmail.com>
2021-03-04 15:11:10 -0800

    Simple Release Script
    This patch adds a simple release script. We intend that this should evenutally be called automatically from CI in the near future.

---
+++ scripts/build_release.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+version="$1"
+
+if [ -z "$version" ]; then
+	echo ""
+	echo "usage: scripts/build_release {version}"
+	echo ""
+	exit 1
+fi
+
+echo "*** Building release $version ***"
+
+cd $(dirname ${BASH_SOURCE[0]})/..
+
+echo "*** Building Solidity ***"
+
+(cd ethereum && yarn install && yarn compile)
+
+contracts="./ethereum/.build/contracts.json"
+
+echo "*** Building release compound chain ***"
+
+cargo build --release
+
+bin="./target/release/compound-chain"
+types="./types.json"
+wasm="./target/release/wbuild/compound-chain-runtime/compound_chain_runtime.compact.wasm"
+
+if [ ! -f "$bin" -o ! -f "$wasm" -o ! -f "$types" -o ! -f "$contracts" ]; then
+	echo "Missing one of the following build files: $bin, $wasm, $types, $contracts"
+	exit 1
+fi
+
+release_dir="./releases/$version"
+
+mkdir -p "$release_dir"
+cp "$wasm" "$release_dir/compound_chain_runtime.compact.wasm"
+cp "$types" "$release_dir/types.json"
+cp "$contracts" "$release_dir/contracts.json"
+
+echo "Built release $version"
+echo "  wasm: $release_dir/compound_chain_runtime.compact.wasm"
+echo "  types: $release_dir/types.json"
+echo "  contracts: $release_dir/contracts.json"
