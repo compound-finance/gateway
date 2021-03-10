2f2c56894e9d007bb575b2dd5f26ec56c370d081
Coburn Berry <coburn@compound.finance>
2021-03-09 19:39:11 -0800

    notes on manual hack of building chianspec

--- scripts/build_spec.sh
+++ scripts/build_spec.sh
@@ -6,10 +6,12 @@ echo "*** Building Chain Spec from staging ***"
 
 cd $(dirname ${BASH_SOURCE[0]})/..
 
-# cargo build --release
-# ./target/release/gateway build-spec --disable-default-bootnode --chain staging > alphaTestnetChainSpec.json
+cargo build --release
+./target/release/gateway build-spec --disable-default-bootnode --chain staging > alphaTestnetChainSpec.json
 #TODO: just put it in chain_spec.rs...
 #TODO: set liquidity factors in script
+#XXX liquidty fators et al are off by several decimal places, since jq was converting them to scientific notation and breaking when hitting rust,
 cat alphaTestnetChainSpec.json | jq --slurpfile asset_config ./scripts/chains/ropsten_token_config.json 'setpath(["genesis", "runtime", "palletCash", "assets"]; $asset_config)' > m.json
+# here, stop and add the needed decimals to the m.json ( or do something smarter )
 ./target/release/gateway build-spec --chain=m.json --raw --disable-default-bootnode > alphaTestnetChainSpecRaw.json
-rm m.json
+# rm m.json
