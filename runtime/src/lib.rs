a799d8ea0cea398728450ebc05bb5b225e7f1b76
Geoffrey Hayes <hayesgm@gmail.com>
2021-03-10 18:56:05 -0800

    Bump to m5
    This patch bumps the chain spec version to 5.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -121,7 +121,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("gateway"),
     impl_name: create_runtime_str!("gateway"),
     authoring_version: 1,
-    spec_version: 4,
+    spec_version: 5,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
