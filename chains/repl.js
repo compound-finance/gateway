fafaa772c4b98a44fabb23fdc3ba2cc0eaabd2b7
Geoffrey Hayes <hayesgm@gmail.com>
2021-03-16 17:32:12 -0700

    Add Gateway REPL
    This patch adds a Gateway repl to make it easy to interact with a Gateway instance. For example, the following examples:
    
    ```sh
    yarn repl -c testnet
    ```
    
    Query the current cash yield
    
    ```sh
    > (await api.query.cash.cashYield()).toJSON()
    300
    ```
    
    Show the current test-net block
    
    ```sh
    > .block
    ```
    
    Show the current test-net validators
    
    ```sh
    > .validators
      5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL:
        substrate_id=0x1cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c
        eth_address=0x286f2a10c28c966e97a72b8246041fbf636e673e
    
      5HMqNs9offzpiebeuzHVwKcJRid1L44KgEKF7jwpYYFM25kY:
        substrate_id=0xea3da7e5b24ee22ce5fa252136745bfbefcb657201404f4479bcbe42135e234c
        eth_address=0x4515e1ce5d4c42da4b0561f52ef12dee19f9c020
    
      5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty:
        substrate_id=0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48
        eth_address=0xc9b0c3ed4efa833a7ad5459755a18f9689a0f7ac
    
      5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY:
        substrate_id=0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
        eth_address=0x55413a2d4908d130c908ccf2f298b235bacd427a
    ```
    
    Show the Starport admin:
    
    ```sh
    > await starport.methods.admin().call()
    '0x046231a12d30248bAD3322Af74CEA9c325627D32'
    ```

---
+++ chains/repl.js
@@ -0,0 +1,327 @@
+const fs = require('fs').promises;
+const repl = require('repl');
+const path = require('path');
+const { Readable } = require('stream');
+const { createReadStream } = require('fs');
+const getopts = require('getopts');
+const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
+const fetch = require('node-fetch');
+const { getSaddle, describeProvider } = require('eth-saddle');
+
+function matchesLine(completion, line) {
+  if (completion.initial && line.startsWith(completion.initial)) {
+    return true;
+  }
+
+  return false;
+}
+
+function targetMatches(completion, line) {
+  let words = line.split(/\s+/);
+  let position = words.length - 1; // e.g. "deploy" = 0 "deploy " = 1 "deploy abc" = 1
+  let lastWord = words.length === 0 ? "" : words[words.length - 1];
+  let targets = completion.targets.filter(({pos}) => pos === position);
+
+  let matching = targets.reduce((acc, {choices}) => {
+    return [
+      ...acc,
+      ...choices.filter((choice) => choice.startsWith(lastWord))
+    ];
+  }, []);
+
+  if (lastWord.length > 0) {
+    return [matching, lastWord];
+  } else {
+    return [matching, line];
+  }
+}
+
+function getCompleter(defaultCompleter, completions) {
+  return function(line, callback) {
+    const lineMatches = completions.filter((completion) => matchesLine(completion, line));
+    let [choices, text] = lineMatches.reduce(([accMatch, accText], completion) => {
+      let [matches, text] = targetMatches(completion, line);
+
+      if (matches && text.length < accText.length) {
+        return [matches, text];
+      } else if (matches && text.length === accText.length) {
+        return [ [ ...accMatch, ...matches ], accText];
+      } else {
+        return [accMatch, accText];
+      }
+    }, [[], line]);
+
+    if (choices.length > 0) {
+      callback(null, [choices, text]);
+    } else {
+      defaultCompleter(line, callback);
+    }
+  }
+}
+
+function getCompletions(defaultCompleter, contracts) {
+  let contractNames = Object.keys(contracts)
+  let contractAddresses = Object.values(contracts).filter((x) => !!x);
+
+  const completions = [
+    {
+      initial: '.deploy',
+      targets: [
+        {
+          pos: 1,
+          choices: contractNames
+        }
+      ]
+    },
+    {
+      initial: '.match',
+      targets: [
+        {
+          pos: 1,
+          choices: contractAddresses
+        },
+        {
+          pos: 2,
+          choices: contractNames
+        }
+      ]
+    }
+  ];
+
+  return getCompleter(defaultCompleter, completions);
+}
+
+function lowerCase(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return str[0].toLowerCase() + str.slice(1,);
+  }
+}
+
+async function wrapError(fn, r) {
+  try {
+    return await fn;
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  } finally {
+    r.displayPrompt();
+  }
+}
+
+async function getContracts(saddle) {
+  let contracts = await saddle.listContracts();
+  let contractInsts = await Object.entries(contracts).reduce(async (acc, [contract, address]) => {
+    if (address) {
+      return {
+        ... await acc,
+        [contract]: await saddle.getContractAt(contract, address)
+      };
+    } else {
+      return await acc;
+    }
+  }, {});
+
+  return {
+    contracts,
+    contractInsts
+  };
+}
+
+function defineAction(r, fn) {
+  return async (name) => {
+    r.clearBufferedCommand();
+    await wrapError(fn(), r);
+  };
+}
+
+function defineCommands(r, { api, keyring }, saddle, network, contracts) {
+  r.defineCommand('validators', {
+    help: 'Show current validators',
+    action: defineAction(r, async () => {
+      let validators = await api.query.cash.validators.entries();
+      validators.forEach(([substrateId, validatorKeys]) => {
+        let key = toSS58(keyring, substrateId.toHuman()[0]);
+        let value = Object.entries(validatorKeys.unwrap().toJSON()).map(([k, v]) =>
+          `\t\t${k}=${v}`).join("\n");
+        console.log(`\t${key}:\n${value}\n`);
+      });
+    })
+  });
+
+  r.defineCommand('block', {
+    help: 'Show current gateway block',
+    action: defineAction(r, async () => {
+      const blockHash = await api.rpc.chain.getBlockHash();
+      const signedBlock = await api.rpc.chain.getBlock(blockHash);
+      let header = signedBlock.block.header;
+      console.log(`#${header.number}`);
+    })
+  });
+
+  r.defineCommand('eth_network', {
+    help: 'Show given Ethereum network',
+    action: defineAction(r, async () => {
+      console.log(`Network: ${network}`);
+    })
+  });
+
+  r.defineCommand('eth_from', {
+    help: 'Show default from Ethereum address',
+    action: defineAction(r, async () => {
+      console.log(`From: ${saddle.network_config.default_account}`);
+    })
+  });
+
+  r.defineCommand('eth_deployed', {
+    help: 'Show given deployed Ethereum contracts',
+    action: defineAction(r, async () => {
+      Object.entries(contracts).forEach(([contract, deployed]) => {
+        console.log(`${contract}: ${deployed || ""}`);
+      });
+    })
+  });
+}
+
+function defineContracts(r, saddle, contractInsts) {
+  Object.entries(contractInsts).forEach(([contract, contractInst]) => {
+    Object.defineProperty(r.context, lowerCase(contract), {
+      configurable: true,
+      enumerable: true,
+      value: contractInst
+    });
+  });
+}
+
+async function loadChainConfig(chain) {
+  return JSON.parse(await fs.readFile(path.join(__dirname, chain, 'chain-config.json'), 'utf8'));
+}
+
+async function loadTypes(version) {
+  return JSON.parse(await fs.readFile(path.join(__dirname, '..', 'releases', `m${Number(version)}`, 'types.json'), 'utf8'));
+}
+
+async function rpc(chain, chainConfig, section, method, params=[]) {
+  if (!chainConfig.rpc) {
+    throw new Error(`No websocket config for chain ${chain}`);
+  }
+
+  let res = await fetch(chainConfig.rpc, {
+    method: 'post',
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: 1,
+      method: `${section}_${method}`,
+      params
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  let resJson = await res.json();
+
+  return resJson.result;
+}
+
+async function getRuntimeVersion(chain, chainConfig) {
+  return await rpc(chain, chainConfig, "state", "getRuntimeVersion");
+}
+
+function toSS58(keyring, arr) {
+  return keyring.encodeAddress(arr);
+}
+
+async function connect(chain, chainConfig) {
+  if (!chainConfig.websocket) {
+    throw new Error(`No websocket config for chain ${chain}`);
+  }
+
+  let runtimeVersion = await getRuntimeVersion(chain, chainConfig);
+  let { specVersion } = runtimeVersion;
+  let typesJson = await loadTypes(specVersion);
+
+  const wsProvider = new WsProvider(chainConfig.websocket);
+  let api = await ApiPromise.create({
+    provider: wsProvider,
+    types: typesJson
+  });
+
+  let keyring = new Keyring();
+  let types = api.types;
+
+  return {
+    wsProvider,
+    api,
+    keyring,
+    types
+  };
+}
+
+function defineKeys(r, obj) {
+  Object.entries(obj).forEach(([key, value]) => {
+    Object.defineProperty(r.context, key, {
+      configurable: false,
+      enumerable: typeof(value) !== 'function',
+      value
+    });
+  });
+}
+
+async function startConsole(input, chain, trace) {
+  let chainConfig = await loadChainConfig(chain);
+  let connection = await connect(chain, chainConfig);
+  let network = chainConfig.eth_network;
+  let saddle = await getSaddle(network);
+  let {contracts, contractInsts} = await getContracts(saddle);
+
+  console.info(`Gateway console on chain ${chain}`);
+
+  Object.entries(contracts).forEach(([contract, deployed]) => {
+    if (deployed) {
+      console.log(`\t${lowerCase(contract)}: ${deployed}`);
+    }
+  });
+
+  let r = repl.start({
+    prompt: '> ',
+    input: input,
+    output: input ? process.stdout : undefined,
+    terminal: input ? false : undefined
+  });
+  if (typeof(r.setupHistory) === 'function') {
+    r.setupHistory(path.join(process.cwd(), '.gateway_history'), (err, repl) => null);
+  }
+  r.originalCompleter = r.completer;
+  r.completer = getCompletions(r.completer, contracts);
+
+  defineCommands(r, connection, saddle, network, contracts);
+
+  defineKeys(r, { saddle });
+  defineKeys(r, connection);
+  defineContracts(r, saddle, contractInsts);
+
+  process.on('uncaughtException', () => console.log('Error'));
+
+  r.on('exit', () => {
+    process.exit();
+  });
+}
+
+let input;
+const options = getopts(process.argv.slice(2), {
+  alias: {
+    script: "s",
+    eval: "e",
+    chain: "c"
+  },
+});
+
+let chain = options.chain || 'testnet';
+if (options.script) {
+  input = createReadStream(options.script);
+} else if (options.eval) {
+  let evalArg = options.eval;
+  let codes = Array.isArray(evalArg) ? evalArg.map((e) => e + ';\n') : [ evalArg ];
+  input = Readable.from(codes);
+}
+
+startConsole(input, chain, process.env['TRACE']);
