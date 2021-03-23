8acce8ba890edd63db08027826e736705f206c8d
maxwolff <maxcwolff@gmail.com>
2021-03-23 15:40:36 -0700

    stub

---
+++ pallets/cash/src/benchmarking.rs
@@ -0,0 +1,21 @@
+#[cfg(feature = "runtime-benchmarks")]
+
+use pallet_cash::{Module as Cash, frame_system};
+pub struct Module<T: Config>(pallet_cash::Module<T>);
+
+mod benchmarking {
+  use frame_benchmarking::{benchmarks, account, impl_benchmark_test_suite};
+  use crate::{*, Module as PalletModule};
+
+  benchmarks!{
+    cull_notices {
+
+    }:_ Cash::cull_notices(RawOrigin::Root)
+}
+
+impl_benchmark_test_suite!(
+	Module,
+	crate::mock::new_test_ext(),
+	crate::mock::Test,
+	extra = false,
+);
