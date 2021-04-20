81cfcce726eed9a7ef30438d99f45ff9ff1f8ab5
Geoff Hayes <hayesgm@gmail.com>
2021-04-19 21:39:25 -0700

    Liquidate Liquidity Check (#284)
    This patch adds a liquidity check to a user to ensure they are underwater before we allow liquidations on the account. There are still other areas to consider (i.e. close factor and collateral quantity), but this is a first start on fixing some bugs in the liquidation function.
    
    Additionally, we add significant test coverage to the `has_liquidity_xxx` functions to ensure they are all individually test in a range of scenarios. We found no serious bugs in that process, but we should consider breaking on some invalid inputs (e.g. for unsupported assets, etc). These are currently left as TODOs.

---
+++ pallets/cash/src/tests/common.rs
@@ -0,0 +1,42 @@
+use crate::tests::{assets::*, *};
+use pallet_oracle::types::Price;
+
+pub fn init_eth_asset() -> Result<ChainAsset, Reason> {
+    pallet_oracle::Prices::insert(ETH.ticker, Price::from_nominal(ETH.ticker, "2000.00").value);
+    SupportedAssets::insert(&Eth, eth);
+
+    Ok(Eth)
+}
+
+pub fn init_uni_asset() -> Result<ChainAsset, Reason> {
+    pallet_oracle::Prices::insert(
+        UNI.ticker,
+        Price::from_nominal(UNI.ticker, "60000.00").value,
+    );
+    SupportedAssets::insert(&Uni, uni);
+
+    Ok(Uni)
+}
+
+pub fn init_wbtc_asset() -> Result<ChainAsset, Reason> {
+    pallet_oracle::Prices::insert(
+        WBTC.ticker,
+        Price::from_nominal(WBTC.ticker, "60000.00").value,
+    );
+    SupportedAssets::insert(&Wbtc, wbtc);
+
+    Ok(Wbtc)
+}
+
+pub fn init_asset_balance(asset: ChainAsset, account: ChainAccount, balance: AssetBalance) {
+    AssetBalances::insert(asset, account, balance);
+    TotalSupplyAssets::insert(
+        asset,
+        (TotalSupplyAssets::get(asset) as i128 + balance) as u128,
+    );
+    AssetsWithNonZeroBalance::insert(account, asset, ());
+}
+
+pub fn init_cash(account: ChainAccount, amount: CashPrincipal) {
+    CashPrincipals::insert(account, amount);
+}
--- pallets/cash/src/tests/mod.rs
+++ pallets/cash/src/tests/mod.rs
@@ -17,45 +17,13 @@ pub use our_std::{iter::FromIterator, str::FromStr};
 pub use mock::*;
 pub mod mock;
 
+pub mod assets;
+pub mod common;
 pub mod extract;
 pub mod protocol;
 pub mod testdata;
 
-pub const ETH: Units = Units::from_ticker_str("ETH", 18);
-pub const Eth: ChainAsset = ChainAsset::Eth(hex!("EeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"));
-pub const eth: AssetInfo = AssetInfo {
-    asset: Eth,
-    decimals: ETH.decimals,
-    liquidity_factor: LiquidityFactor::from_nominal("0.8"),
-    rate_model: InterestRateModel::Kink {
-        zero_rate: APR(0),
-        kink_rate: APR(200),
-        kink_utilization: Factor::from_nominal("0.9"),
-        full_rate: APR(5000),
-    },
-    miner_shares: Factor::from_nominal("0.05"),
-    supply_cap: Quantity::from_nominal("1000", ETH).value,
-    symbol: Symbol(ETH.ticker.0),
-    ticker: Ticker(ETH.ticker.0),
-};
-
-pub const UNI: Units = Units::from_ticker_str("UNI", 18);
-pub const Uni: ChainAsset = ChainAsset::Eth(hex!("1f9840a85d5af5bf1d1762f925bdaddc4201f984"));
-pub const uni: AssetInfo = AssetInfo {
-    asset: Uni,
-    decimals: UNI.decimals,
-    liquidity_factor: LiquidityFactor::from_nominal("0.7"),
-    rate_model: InterestRateModel::Kink {
-        zero_rate: APR(0),
-        kink_rate: APR(500),
-        kink_utilization: Factor::from_nominal("0.8"),
-        full_rate: APR(2000),
-    },
-    miner_shares: Factor::from_nominal("0.05"),
-    supply_cap: Quantity::from_nominal("1000", UNI).value,
-    symbol: Symbol(UNI.ticker.0),
-    ticker: Ticker(UNI.ticker.0),
-};
+pub use assets::*;
 
 #[macro_export]
 macro_rules! bal {
