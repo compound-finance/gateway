59109359af7929d7e5ac0dd3e3cd39cfab2577a7
Geoffrey Hayes <hayesgm@gmail.com>
2021-04-21 00:09:43 -0700

    With fees

--- pallets/cash/src/effectful.rs
+++ pallets/cash/src/effectful.rs
@@ -6,13 +6,16 @@ use our_std::RuntimeDebug;
 
 use crate::{
     chains::ChainAccount,
+    core::get_some_miner,
     internal::balance_helpers::*,
     portfolio::Portfolio,
     reason::Reason,
-    types::{AssetBalance, AssetIndex, AssetInfo, Balance, CashPrincipal, Quantity},
+    types::{
+        AssetBalance, AssetIndex, AssetInfo, Balance, CashPrincipal, CashPrincipalAmount, Quantity,
+    },
     AssetAmount, AssetBalances, AssetsWithNonZeroBalance, BorrowIndices, CashPrincipals,
     ChainAsset, Config, GlobalCashIndex, LastIndices, SupplyIndices, SupportedAssets,
-    TotalBorrowAssets, TotalSupplyAssets,
+    TotalBorrowAssets, TotalCashPrincipal, TotalSupplyAssets,
 };
 
 trait Apply {
@@ -27,6 +30,7 @@ pub struct State {
     assets_with_non_zero_balance: BTreeMap<(ChainAsset, ChainAccount), bool>,
     last_indices: BTreeMap<(ChainAsset, ChainAccount), AssetIndex>,
     cash_principals: BTreeMap<ChainAccount, CashPrincipal>,
+    total_cash_principal: Option<CashPrincipalAmount>,
 }
 
 impl State {
@@ -38,6 +42,7 @@ impl State {
             assets_with_non_zero_balance: BTreeMap::new(),
             last_indices: BTreeMap::new(),
             cash_principals: BTreeMap::new(),
+            total_cash_principal: None,
         }
     }
 
@@ -193,6 +198,18 @@ impl State {
         self.cash_principals.insert(account, cash_principal);
     }
 
+    pub fn get_total_cash_principal<T: Config>(self: &Self) -> CashPrincipalAmount {
+        self.total_cash_principal
+            .unwrap_or_else(|| TotalCashPrincipal::get())
+    }
+
+    pub fn set_total_cash_principal<T: Config>(
+        self: &mut Self,
+        total_cash_principal: CashPrincipalAmount,
+    ) {
+        self.total_cash_principal = Some(total_cash_principal);
+    }
+
     pub fn commit<T: Config>(self: Self) {
         self.total_supply_asset
             .into_iter()
@@ -231,6 +248,7 @@ impl State {
     }
 }
 
+// TODO: require!(sender != recipient, Reason::SelfTransfer);
 fn prepare_transfer_asset<T: Config>(
     st_pre: &State,
     sender: ChainAccount,
@@ -239,6 +257,10 @@ fn prepare_transfer_asset<T: Config>(
     quantity: Quantity,
 ) -> Result<State, Reason> {
     let mut st: State = st_pre.clone();
+
+    if (sender == recipient) {
+        Err(Reason::SelfTransfer)?
+    }
 
     let asset_info = SupportedAssets::get(asset).ok_or(Reason::AssetNotSupported)?;
     let supply_index = SupplyIndices::get(asset);
@@ -252,10 +274,10 @@ fn prepare_transfer_asset<T: Config>(
     let sender_cash_principal_pre = st.get_cash_principal::<T>(sender);
     let recipient_cash_principal_pre = st.get_cash_principal::<T>(recipient);
 
-    let (recipient_repay_amount, recipient_supply_amount) =
-        repay_and_supply_amount(recipient_balance_pre.value, quantity)?;
     let (sender_withdraw_amount, sender_borrow_amount) =
         withdraw_and_borrow_amount(sender_balance_pre.value, quantity)?;
+    let (recipient_repay_amount, recipient_supply_amount) =
+        repay_and_supply_amount(recipient_balance_pre.value, quantity)?;
 
     let total_supply_new = total_supply_pre
         .add(recipient_supply_amount)?
@@ -300,6 +322,47 @@ fn prepare_transfer_asset<T: Config>(
     Ok(st)
 }
 
+fn prepare_transfer_cash<T: Config>(
+    st_pre: &State,
+    sender: ChainAccount,
+    recipient: ChainAccount,
+    principal: CashPrincipalAmount,
+    fee_principal: CashPrincipalAmount,
+) -> Result<State, Reason> {
+    let mut st: State = st_pre.clone();
+    let miner = get_some_miner::<T>();
+
+    let principal_with_fee = principal.add(fee_principal)?;
+
+    let sender_cash_pre = st.get_cash_principal::<T>(sender);
+    let recipient_cash_pre = st.get_cash_principal::<T>(recipient);
+    let miner_cash_pre = st.get_cash_principal::<T>(miner);
+
+    let (_sender_withdraw_principal, sender_borrow_principal) =
+        withdraw_and_borrow_principal(sender_cash_pre, principal_with_fee)?;
+    let (recipient_repay_principal, _recipient_supply_principal) =
+        repay_and_supply_principal(recipient_cash_pre, principal)?;
+    let (miner_repay_principal, _miner_supply_principal) =
+        repay_and_supply_principal(miner_cash_pre, fee_principal)?;
+
+    let miner_cash_post = miner_cash_pre.add_amount(fee_principal)?;
+    let sender_cash_post = sender_cash_pre.sub_amount(principal_with_fee)?;
+    let recipient_cash_post = recipient_cash_pre.add_amount(principal)?;
+
+    let total_cash_post = st
+        .get_total_cash_principal::<T>()
+        .add(sender_borrow_principal)?
+        .sub(recipient_repay_principal)?
+        .sub(miner_repay_principal)?;
+
+    st.set_cash_principal::<T>(miner, miner_cash_post);
+    st.set_cash_principal::<T>(sender, sender_cash_post);
+    st.set_cash_principal::<T>(recipient, recipient_cash_post);
+    st.set_total_cash_principal::<T>(total_cash_post);
+
+    Ok(st)
+}
+
 #[derive(Clone, Eq, PartialEq, RuntimeDebug)]
 pub enum Effect {
     Init,
@@ -338,7 +401,7 @@ impl Effectful {
         }
     }
 
-    fn current_state(self: &Self) -> &State {
+    fn state(self: &Self) -> &State {
         &self.effects.last().expect("expected last state").1
     }
 
@@ -355,10 +418,32 @@ impl Effectful {
             asset,
             quantity,
         };
-        let state_old = self.current_state();
+        let state_old = self.state();
         let state_new = effect.apply::<T>(state_old)?;
         self.effects.push((effect, state_new));
         Ok(self)
+    }
+
+    pub fn check_liquidity<T: Config>(
+        self: &mut Self,
+        account: ChainAccount,
+    ) -> Result<&mut Self, Reason> {
+        // TODO: Consider removing all the state cloning
+        let liquidity = self
+            .state()
+            .clone()
+            .build_portfolio::<T>(account)?
+            .get_liquidity::<T>()?;
+        if liquidity.value < 0 {
+            Err(Reason::InsufficientLiquidity)?
+        } else {
+            Ok(self)
+        }
+    }
+
+    pub fn commit<T: Config>(self: &mut Self) {
+        // TODO: Consider removing all the state cloning
+        self.state().clone().commit::<T>();
     }
 }
 
@@ -395,7 +480,6 @@ mod tests {
         tests::{assert_ok, assets::*, common::*, mock::*},
         types::*,
     };
-    use pallet_oracle::types::Price;
 
     #[allow(non_upper_case_globals)]
     const account_a: ChainAccount = ChainAccount::Eth([1u8; 20]);
@@ -403,7 +487,7 @@ mod tests {
     const account_b: ChainAccount = ChainAccount::Eth([2u8; 20]);
 
     #[test]
-    fn test_transfer_asset_success() {
+    fn test_transfer_asset_success_state() {
         new_test_ext().execute_with(|| {
             assert_ok!(init_eth_asset());
 
@@ -413,10 +497,9 @@ mod tests {
             let state = Effectful::new()
                 .transfer_asset::<Test>(account_a, account_b, Eth, quantity)
                 .expect("transfer_asset failed")
-                .current_state()
+                .state()
                 .clone();
 
-            println!("State={:?}", state);
             assert_eq!(
                 state,
                 State {
@@ -444,6 +527,298 @@ mod tests {
                     .into_iter()
                     .collect(),
                 }
+            );
+        })
+    }
+
+    #[test]
+    fn test_build_portfolio() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(init_eth_asset());
+            assert_ok!(init_wbtc_asset());
+
+            CashPrincipals::insert(account_a, CashPrincipal(10000000)); // 10 CASH
+            AssetsWithNonZeroBalance::insert(account_a, Wbtc, ());
+
+            let eth_quantity = eth.as_quantity_nominal("1");
+            let eth_amount = eth_quantity.value as i128;
+
+            let wbtc_quantity = wbtc.as_quantity_nominal("0.02");
+            let wbtc_amount = wbtc_quantity.value as i128;
+
+            let state = Effectful::new()
+                .transfer_asset::<Test>(account_a, account_b, Eth, eth_quantity)
+                .expect("transfer_asset(eth) failed")
+                .transfer_asset::<Test>(account_b, account_a, Wbtc, wbtc_quantity)
+                .expect("transfer_asset(wbtc) failed")
+                .state()
+                .clone();
+
+            let portfolio_a = state.clone().build_portfolio::<Test>(account_a);
+
+            assert_eq!(
+                portfolio_a,
+                Ok(Portfolio {
+                    cash: Balance {
+                        value: 10000000,
+                        units: CASH
+                    },
+                    positions: vec![
+                        (
+                            wbtc,
+                            Balance {
+                                value: wbtc_amount,
+                                units: WBTC
+                            }
+                        ),
+                        (
+                            eth,
+                            Balance {
+                                value: -eth_amount,
+                                units: ETH
+                            }
+                        ),
+                    ]
+                })
+            );
+
+            let portfolio_b = state.build_portfolio::<Test>(account_b);
+
+            assert_eq!(
+                portfolio_b,
+                Ok(Portfolio {
+                    cash: Balance {
+                        value: 0,
+                        units: CASH
+                    },
+                    positions: vec![
+                        (
+                            wbtc,
+                            Balance {
+                                value: -wbtc_amount,
+                                units: WBTC
+                            }
+                        ),
+                        (
+                            eth,
+                            Balance {
+                                value: eth_amount,
+                                units: ETH
+                            }
+                        ),
+                    ]
+                })
+            );
+        })
+    }
+
+    #[test]
+    fn test_check_liquidity() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(init_eth_asset());
+            assert_ok!(init_wbtc_asset());
+
+            CashPrincipals::insert(account_a, CashPrincipal(100000000000)); // 100,000 CASH
+            AssetsWithNonZeroBalance::insert(account_a, Wbtc, ());
+
+            let eth_quantity = eth.as_quantity_nominal("1");
+            let wbtc_quantity = wbtc.as_quantity_nominal("0.02");
+
+            let mut effect = Effectful::new()
+                .transfer_asset::<Test>(account_a, account_b, Eth, eth_quantity)
+                .expect("transfer_asset(eth) failed")
+                .transfer_asset::<Test>(account_b, account_a, Wbtc, wbtc_quantity)
+                .expect("transfer_asset(wbtc) failed")
+                .clone();
+
+            assert_ok!(effect.check_liquidity::<Test>(account_a));
+            assert_eq!(
+                effect.check_liquidity::<Test>(account_b),
+                Err(Reason::InsufficientLiquidity)
+            );
+        })
+    }
+
+    #[test]
+    fn test_transfer_two_assets_success_state() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(init_eth_asset());
+            assert_ok!(init_wbtc_asset());
+
+            let eth_quantity = eth.as_quantity_nominal("1");
+            let eth_amount = eth_quantity.value as i128;
+
+            let wbtc_quantity = wbtc.as_quantity_nominal("0.1");
+            let wbtc_amount = wbtc_quantity.value as i128;
+
+            let state = Effectful::new()
+                .transfer_asset::<Test>(account_a, account_b, Eth, eth_quantity)
+                .expect("transfer_asset(eth) failed")
+                .transfer_asset::<Test>(account_b, account_a, Wbtc, wbtc_quantity)
+                .expect("transfer_asset(wbtc) failed")
+                .state()
+                .clone();
+
+            assert_eq!(
+                state,
+                State {
+                    total_supply_asset: vec![
+                        (Eth, eth_quantity.value),
+                        (Wbtc, wbtc_quantity.value)
+                    ]
+                    .into_iter()
+                    .collect(),
+                    total_borrow_asset: vec![
+                        (Eth, eth_quantity.value),
+                        (Wbtc, wbtc_quantity.value)
+                    ]
+                    .into_iter()
+                    .collect(),
+                    asset_balances: vec![
+                        ((Eth, account_a), -eth_amount),
+                        ((Eth, account_b), eth_amount),
+                        ((Wbtc, account_a), wbtc_amount),
+                        ((Wbtc, account_b), -wbtc_amount)
+                    ]
+                    .into_iter()
+                    .collect(),
+                    assets_with_non_zero_balance: vec![
+                        ((Eth, account_a), true),
+                        ((Eth, account_b), true),
+                        ((Wbtc, account_a), true),
+                        ((Wbtc, account_b), true)
+                    ]
+                    .into_iter()
+                    .collect(),
+                    last_indices: vec![
+                        ((Eth, account_a), AssetIndex::from_nominal("0")),
+                        ((Eth, account_b), AssetIndex::from_nominal("0")),
+                        ((Wbtc, account_a), AssetIndex::from_nominal("0")),
+                        ((Wbtc, account_b), AssetIndex::from_nominal("0"))
+                    ]
+                    .into_iter()
+                    .collect(),
+                    cash_principals: vec![
+                        (account_a, CashPrincipal::from_nominal("0")),
+                        (account_b, CashPrincipal::from_nominal("0")),
+                    ]
+                    .into_iter()
+                    .collect(),
+                }
+            );
+        })
+    }
+
+    #[test]
+    fn test_transfer_asset_success_commit() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(init_eth_asset());
+
+            let quantity = eth.as_quantity_nominal("1");
+            let amount = quantity.value as i128;
+
+            Effectful::new()
+                .transfer_asset::<Test>(account_a, account_b, Eth, quantity)
+                .expect("transfer_asset failed")
+                .commit::<Test>();
+
+            assert_eq!(TotalSupplyAssets::get(Eth), quantity.value);
+            assert_eq!(TotalBorrowAssets::get(Eth), quantity.value);
+            assert_eq!(AssetBalances::get(Eth, account_a), -amount);
+            assert_eq!(AssetBalances::get(Eth, account_b), amount);
+            assert_eq!(
+                AssetsWithNonZeroBalance::iter_prefix(account_a).collect::<Vec<_>>(),
+                vec![(Eth, ())]
+            );
+            assert_eq!(
+                AssetsWithNonZeroBalance::iter_prefix(account_b).collect::<Vec<_>>(),
+                vec![(Eth, ())]
+            );
+            assert_eq!(
+                LastIndices::get(Eth, account_a),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                LastIndices::get(Eth, account_b),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                LastIndices::get(Wbtc, account_a),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                LastIndices::get(Wbtc, account_b),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                CashPrincipals::get(account_a),
+                CashPrincipal::from_nominal("0")
+            );
+            assert_eq!(
+                CashPrincipals::get(account_b),
+                CashPrincipal::from_nominal("0")
+            );
+        })
+    }
+
+    #[test]
+    fn test_transfer_two_assets_success_commit() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(init_eth_asset());
+            assert_ok!(init_wbtc_asset());
+
+            let eth_quantity = eth.as_quantity_nominal("1");
+            let eth_amount = eth_quantity.value as i128;
+
+            let wbtc_quantity = wbtc.as_quantity_nominal("0.1");
+            let wbtc_amount = wbtc_quantity.value as i128;
+
+            Effectful::new()
+                .transfer_asset::<Test>(account_a, account_b, Eth, eth_quantity)
+                .expect("transfer_asset(eth) failed")
+                .transfer_asset::<Test>(account_b, account_a, Wbtc, wbtc_quantity)
+                .expect("transfer_asset(wbtc) failed")
+                .commit::<Test>();
+
+            assert_eq!(TotalSupplyAssets::get(Eth), eth_quantity.value);
+            assert_eq!(TotalBorrowAssets::get(Eth), eth_quantity.value);
+            assert_eq!(TotalSupplyAssets::get(Wbtc), wbtc_quantity.value);
+            assert_eq!(TotalBorrowAssets::get(Wbtc), wbtc_quantity.value);
+            assert_eq!(AssetBalances::get(Eth, account_a), -eth_amount);
+            assert_eq!(AssetBalances::get(Eth, account_b), eth_amount);
+            assert_eq!(AssetBalances::get(Wbtc, account_a), wbtc_amount);
+            assert_eq!(AssetBalances::get(Wbtc, account_b), -wbtc_amount);
+            assert_eq!(
+                AssetsWithNonZeroBalance::iter_prefix(account_a).collect::<Vec<_>>(),
+                vec![(Eth, ()), (Wbtc, ())]
+            );
+            assert_eq!(
+                AssetsWithNonZeroBalance::iter_prefix(account_b).collect::<Vec<_>>(),
+                vec![(Eth, ()), (Wbtc, ())]
+            );
+            assert_eq!(
+                LastIndices::get(Eth, account_a),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                LastIndices::get(Eth, account_b),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                LastIndices::get(Wbtc, account_a),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                LastIndices::get(Wbtc, account_b),
+                AssetIndex::from_nominal("0")
+            );
+            assert_eq!(
+                CashPrincipals::get(account_a),
+                CashPrincipal::from_nominal("0")
+            );
+            assert_eq!(
+                CashPrincipals::get(account_b),
+                CashPrincipal::from_nominal("0")
             );
         })
     }
