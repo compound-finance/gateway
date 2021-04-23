94b5c058dca8c1e65a2f4bec99ef34bfd0766d5e
Coburn Berry <32463466+coburncoburn@users.noreply.github.com>
2021-04-23 14:07:44 -0700

    add trx request to the and_provides to fix stuck nonces on failed trx… (#301)
    * add trx request to the and_provides to fix stuck nonces on failed trx requests
    
    * fix test

--- pallets/cash/src/internal/validate_trx.rs
+++ pallets/cash/src/internal/validate_trx.rs
@@ -106,6 +106,7 @@ pub fn validate_unsigned<T: Config>(
                                 .priority(UNSIGNED_TXS_PRIORITY)
                                 .longevity(UNSIGNED_TXS_LONGEVITY)
                                 .and_provides((sender, nonce))
+                                .and_provides(request)
                                 .propagate(true)
                                 .build(),
                         )
@@ -116,6 +117,7 @@ pub fn validate_unsigned<T: Config>(
                                 .longevity(UNSIGNED_TXS_LONGEVITY)
                                 .and_requires((sender, nonce - 1))
                                 .and_provides((sender, nonce))
+                                .and_provides(request)
                                 .propagate(true)
                                 .build(),
                         )
@@ -321,6 +323,7 @@ mod tests {
                 .priority(100)
                 .longevity(32)
                 .and_provides((ChainAccount::Eth(eth_address), 0))
+                .and_provides(request.clone())
                 .propagate(true)
                 .build();
 
@@ -360,6 +363,7 @@ mod tests {
                 .priority(UNSIGNED_TXS_PRIORITY)
                 .longevity(UNSIGNED_TXS_LONGEVITY)
                 .and_provides((ChainAccount::Eth(eth_address), 5))
+                .and_provides(request.clone())
                 .propagate(true)
                 .build();
 
@@ -399,6 +403,7 @@ mod tests {
                 .longevity(UNSIGNED_TXS_LONGEVITY)
                 .and_requires((ChainAccount::Eth(eth_address), nonce - 1))
                 .and_provides((ChainAccount::Eth(eth_address), nonce))
+                .and_provides(request.clone())
                 .propagate(true)
                 .build();
 
