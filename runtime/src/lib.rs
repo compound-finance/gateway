02b7903a13ea90bbd8afb6614f6d5ddec55c2625
Coburn Berry <coburn@compound.finance>
2021-04-27 12:51:13 -0700

    bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -119,7 +119,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("gateway"),
     impl_name: create_runtime_str!("gateway"),
     authoring_version: 1,
-    spec_version: 8,
+    spec_version: 9,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
