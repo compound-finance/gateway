84f1932422f598dc8a2493cd71e6838d2b65d849
Antonina Norair <antonina.norair@gmail.com>
2021-06-02 16:01:22 -0700

    Get rid of all hardcoded signatures

--- flow/test/starport.test.js
+++ flow/test/starport.test.js
@@ -23,6 +23,21 @@ import { signWithKey } from "./flow.signer";
 const basePath = path.resolve(__dirname, "../cadence");
 const STARPORT_CONTRACT_NAME = "Starport";
 
+const AUTHORITIES_DATA = [
+  {
+    publicKey:
+      "b82a83577dd93a351d980dc8e55b378480ac552f7e83d66548a14219e10b52a5a7bb4840b50bfdf68df0e27b2a130a6bed4f5b695ad2e8b628be5b51e41aa6b9",
+    privateKey:
+      "57566b147fb5431230d8ac3ecf0432993b26510bbc82c8c7f6bae56705dfb3ea",
+  },
+  {
+    publicKey:
+      "52165a32bd7bf883837a96b9dbec1a004d3a44f43eac2eba2ff9e6940364b64733cd20dd80d66d367de8bcf1875bfb0b2a7c9beb451572c451d6a6882c72677f",
+    privateKey:
+      "ae732bb18cbb6705e6f63fa4ccfb89afd8169181bb39482d857102b3dca4fe67",
+  },
+];
+
 async function deployStarport() {
   const deployer = await getAccountAddress(); // random address
 
@@ -71,10 +86,7 @@ async function prepareForUnlock(userName) {
   // User deposits Flow token to Starport
   await depositFlowTokens(user, "100.000000");
 
-  const authorities = [
-    "b82a83577dd93a351d980dc8e55b378480ac552f7e83d66548a14219e10b52a5a7bb4840b50bfdf68df0e27b2a130a6bed4f5b695ad2e8b628be5b51e41aa6b9",
-    "52165a32bd7bf883837a96b9dbec1a004d3a44f43eac2eba2ff9e6940364b64733cd20dd80d66d367de8bcf1875bfb0b2a7c9beb451572c451d6a6882c72677f",
-  ];
+  const authorities = [AUTHORITIES_DATA[0].publicKey, AUTHORITIES_DATA[1].publicKey];
   await runTransaction("starport/change_authorities_admin", address.Starport, [
     [authorities, fArray(fString)],
   ]);
@@ -261,10 +273,16 @@ describe("Starport Tests", () => {
       amount
     );
     // Sign the message with Authority 1 private key
-    const signature1 = signWithKey("57566b147fb5431230d8ac3ecf0432993b26510bbc82c8c7f6bae56705dfb3ea", unlockMessage)
+    const signature1 = signWithKey(
+      AUTHORITIES_DATA[0].privateKey,
+      unlockMessage
+    );
     // Sign the message with Authority 2 private key
-    const signature2 = signWithKey("ae732bb18cbb6705e6f63fa4ccfb89afd8169181bb39482d857102b3dca4fe67", unlockMessage)
-    const signatures = [signature1, signature2]
+    const signature2 = signWithKey(
+      AUTHORITIES_DATA[1].privateKey,
+      unlockMessage
+    );
+    const signatures = [signature1, signature2];
 
     const userBalanceBefore = Number(await getAccountFlowBalance(toAddress));
     const unlockRes = await runTransaction(
@@ -351,8 +369,8 @@ describe("Starport Tests", () => {
     const address = await getAddressMap();
 
     const authorities = [
-      "b82a83577dd93a351d980dc8e55b378480ac552f7e83d66548a14219e10b52a5a7bb4840b50bfdf68df0e27b2a130a6bed4f5b695ad2e8b628be5b51e41aa6b9",
-      "52165a32bd7bf883837a96b9dbec1a004d3a44f43eac2eba2ff9e6940364b64733cd20dd80d66d367de8bcf1875bfb0b2a7c9beb451572c451d6a6882c72677f",
+      AUTHORITIES_DATA[0].publicKey,
+      AUTHORITIES_DATA[1].publicKey,
     ];
     await runTransaction(
       "starport/change_authorities_admin",
@@ -367,15 +385,29 @@ describe("Starport Tests", () => {
     const noticeEraId = 1;
     const noticeEraIndex = 0;
     const parentHex = "";
-    const signatures = [
-      "1b932a5a26ff58a88df0346bb02c6e082ceabb0f4e9e8bcc06b3f22d54b59bea7fca0e67e89d8a9c6d64627bdee7c93ba1bb4e2a8a6a5f6001bf10f43183965e",
-      "31687ba03b54e936339d7a9946ceff183ac5059f36936e12fc1d355cf4c18bad12006aaaab9197250b1a521d388a8fcb975c27b4de0d9be205c332e2f1d26390",
-    ];
 
     const newAuthorities = [
       "05df808dce3bf02d37990bd76a6e4deaaf5e29ac03677227d42b0d6914403d626256a30fb15d80da9aad7d2b22ffc5a8998043dcf86c38b3d03ea784a33d441a",
       "b97f907e17fcc7cdb98fb8952afbe6c610d78e969336e3577190a10cf4629dc25398d737b67cb0249c8da8bf191ee36686aed9e8172fd90d397d704dc1110ae6",
     ];
+
+    const changeAuthoritiesMessage = await buildChangeAuthoritiesMessage(
+      noticeEraId,
+      noticeEraIndex,
+      parentHex,
+      newAuthorities
+    );
+    // Sign the message with Authority 1 private key
+    const signature1 = signWithKey(
+      AUTHORITIES_DATA[0].privateKey,
+      changeAuthoritiesMessage
+    );
+    // Sign the message with Authority 2 private key
+    const signature2 = signWithKey(
+      AUTHORITIES_DATA[1].privateKey,
+      changeAuthoritiesMessage
+    );
+    const signatures = [signature1, signature2];
 
     const changeAuthoritiesRes = await runTransaction(
       "starport/change_authorities_notice",
@@ -431,8 +463,8 @@ describe("Starport Tests", () => {
     const address = await getAddressMap();
 
     const authorities = [
-      "b82a83577dd93a351d980dc8e55b378480ac552f7e83d66548a14219e10b52a5a7bb4840b50bfdf68df0e27b2a130a6bed4f5b695ad2e8b628be5b51e41aa6b9",
-      "52165a32bd7bf883837a96b9dbec1a004d3a44f43eac2eba2ff9e6940364b64733cd20dd80d66d367de8bcf1875bfb0b2a7c9beb451572c451d6a6882c72677f",
+      AUTHORITIES_DATA[0].publicKey,
+      AUTHORITIES_DATA[1].publicKey,
     ];
     await runTransaction(
       "starport/change_authorities_admin",
@@ -447,12 +479,26 @@ describe("Starport Tests", () => {
     const noticeEraId = 1;
     const noticeEraIndex = 0;
     const parentHex = "";
-    const signatures = [
-      "7044170bba842f04364cabf8d3b365894f387247546cac68e90ad381d629dc215715397d1f2f44a6bdd6575b9d2d7f30e1fea54739f60decd9a249e6dae63361",
-      "5947a87c87e66e5fa61f22d5734ca7a2d7491da4250f22b083976a9f0e14a965b58e10cf4c9d5e1e19377ff58b14b33feb6a5fc085da5578fd0bef773a269e7e",
-    ];
-
     const newSupplyCap = "1000.0";
+
+    const setSupplyCapMessage = await buildSetSupplyCapMessage(
+      noticeEraId,
+      noticeEraIndex,
+      parentHex,
+      newSupplyCap
+    );
+    // Sign the message with Authority 1 private key
+    const signature1 = signWithKey(
+      AUTHORITIES_DATA[0].privateKey,
+      setSupplyCapMessage
+    );
+    // Sign the message with Authority 2 private key
+    const signature2 = signWithKey(
+      AUTHORITIES_DATA[1].privateKey,
+      setSupplyCapMessage
+    );
+    const signatures = [signature1, signature2];
+
     const setSupplyCapRes = await runTransaction(
       "starport/set_supply_cap_notice",
       address.Starport,
@@ -474,7 +520,7 @@ describe("Starport Tests", () => {
     expect(Number(setSupplyCap)).toEqual(Number(newSupplyCap));
   });
 
-  test("# Set supply cap", async () => {
+  test("# Set supply cap by admin", async () => {
     await deployStarport();
 
     const address = await getAddressMap();
@@ -532,14 +578,28 @@ describe("Starport Tests", () => {
     const noticeEraId = 1;
     const noticeEraIndex = 0;
     const parentHex = "";
-    const signatures = [
-      "f8661ebbbe0cc415063a6027fadf6d78b883b88f0ea3b7a15d839b126aa85b55b273e2aeb777a07d04288b432897409e50d4cbadb28f4cf072c5bd3b9220d30e",
-      "701e292593ef04dacc9d35090182b831197fbae7b900585d822b901aa05df75ff505e167a3e2504cc2a0bd928507eaa3e81b9dfbd6878ec9d2e121f0b69537c8",
-    ];
-
     // Unlock tokens to the given address
     const toAddress = "0xf3fcd2c1a78f5eee";
     const amount = "10.0";
+
+    const unlockMessage = await buildUnlockMessage(
+      noticeEraId,
+      noticeEraIndex,
+      parentHex,
+      toAddress,
+      amount
+    );
+    // Sign the message with Authority 1 private key
+    const signature1 = signWithKey(
+      AUTHORITIES_DATA[0].privateKey,
+      unlockMessage
+    );
+    // Sign the message with Authority 2 private key
+    const signature2 = signWithKey(
+      AUTHORITIES_DATA[1].privateKey,
+      unlockMessage
+    );
+    const signatures = [signature1, signature2];
 
     await runTransaction(
       "starport/unlock_flow_tokens_notice",
@@ -583,14 +643,13 @@ describe("Starport Tests", () => {
     const noticeEraId = 1;
     const noticeEraIndex = 0;
     const parentHex = "";
+    // Unlock tokens to the given address
+    const toAddress = "0xf3fcd2c1a78f5eee";
+    const amount = "10.0";
     const signatures = [
       "d8661ebbbe0cc415063a6027fadf6d78b883b88f0ea3b7a15d839b126aa85b55b273e2aeb777a07d04288b432897409e50d4cbadb28f4cf072c5bd3b9220d30e",
       "f01e292593ef04dacc9d35090182b831197fbae7b900585d822b901aa05df75ff505e167a3e2504cc2a0bd928507eaa3e81b9dfbd6878ec9d2e121f0b69537c8",
     ];
-
-    // Unlock tokens to the given address
-    const toAddress = "0xf3fcd2c1a78f5eee";
-    const amount = "10.0";
 
     const unlockRes = await runTransaction(
       "starport/unlock_flow_tokens_notice",
@@ -620,14 +679,28 @@ describe("Starport Tests", () => {
     const noticeEraId = 3;
     const noticeEraIndex = 0;
     const parentHex = "";
-    const signatures = [
-      "79fd383f27b4bb2a853e9bd968f81ae4b47c0aeecdd5f62f2ff3f2f18028c4409587e2d701c6707cb1c98ff0c39d7506146d6234466b4e6475a978aeb7bb0a98",
-      "7654e3c9761c589df7e5fe0c325b73273eb8b1ed9a68ec40985ce8aa8b67445bd322a321340815ccb5befe5c5ce22c74c1a8b44e8137cd5b55d2b5b40c1618bf",
-    ];
-
     // Unlock tokens to the given address
     const toAddress = "0x179b6b1cb6755e31";
     const amount = "10.0";
+
+    const unlockMessage = await buildUnlockMessage(
+      noticeEraId,
+      noticeEraIndex,
+      parentHex,
+      toAddress,
+      amount
+    );
+    // Sign the message with Authority 1 private key
+    const signature1 = signWithKey(
+      AUTHORITIES_DATA[0].privateKey,
+      unlockMessage
+    );
+    // Sign the message with Authority 2 private key
+    const signature2 = signWithKey(
+      AUTHORITIES_DATA[1].privateKey,
+      unlockMessage
+    );
+    const signatures = [signature1, signature2];
 
     const unlockErrRes = await runTransaction(
       "starport/unlock_flow_tokens_notice",
